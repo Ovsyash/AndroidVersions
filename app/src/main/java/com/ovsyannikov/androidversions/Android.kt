2a951e497630852c964ffeb78937359ace27c31d
Ovsyash <r1sk-57@mail.ru>
2022-08-24 12:39:53 +0300

    + Fragment

--- app/src/main/java/com/ovsyannikov/androidversions/Android.kt
+++ app/src/main/java/com/ovsyannikov/androidversions/Android.kt
@@ -1,10 +1,15 @@
 package com.ovsyannikov.androidversions
 
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Android(
     val title: String,
-    @DrawableRes val imageAndroid: Int
-) {
-
-}
+    @DrawableRes val imageAndroid: Int,
+    @DrawableRes val posterAndroid: Int,
+    val overview: String,
+    val releaseDate: String,
+    val trailerIrl: String
+) : Parcelable
--- app/src/main/java/com/ovsyannikov/androidversions/AndroidAdapter.kt
+++ app/src/main/java/com/ovsyannikov/androidversions/AndroidAdapter.kt
@@ -8,15 +8,28 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class AndroidAdapter(Context: Context, private val android: List<Android>) :
-    RecyclerView.Adapter<AndroidAdapter.ViewHolder>() {
+class AndroidAdapter(
+    Context: Context, private val android: List<Android>,
+    private val clickListener: (position: Int) -> Unit
+) : RecyclerView.Adapter<AndroidAdapter.ViewHolder>() {
 
     private val inflater = LayoutInflater.from(Context)
 
-    class ViewHolder(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(
+        itemView: View,
+        listener: (position: Int) -> Unit
+    ) : RecyclerView.ViewHolder(itemView) {
         private val androidImage: ImageView = itemView.findViewById(R.id.androidImage)
         private val androidText: TextView = itemView.findViewById(R.id.androidText)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener(position)
+                }
+            }
+        }
 
         fun bind(version: Android) {
             androidImage.setImageResource(version.imageAndroid)
@@ -26,7 +39,7 @@ class AndroidAdapter(Context: Context, private val android: List<Android>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(inflater.inflate(R.layout.item_android, parent, false))
+        return ViewHolder(inflater.inflate(R.layout.item_android, parent, false), clickListener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
